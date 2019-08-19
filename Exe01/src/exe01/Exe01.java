fb14ad62ef50436fa0a9c9345354497a677de868
FelipeAN26 <54293258+FelipeAN26@users.noreply.github.com>
2019-08-19 19:54:09 -0300

    Inicialização Projeto01AloMundo-Java

---
+++ Exe01/src/exe01/Exe01.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exe01;
+
+/**
+ *
+ * @author aluno
+ */
+public class Exe01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Alo Mundo Java!");
+    }
+    
+}
